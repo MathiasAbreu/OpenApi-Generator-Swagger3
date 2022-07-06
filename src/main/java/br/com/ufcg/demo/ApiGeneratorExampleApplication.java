90e8023eadfd63d74a3b5ddcea1691c714a3283e
Leticia Gabriela Calixto <55746211+Leticiagc@users.noreply.github.com>
2022-07-06 11:21:35 -0300

    first commit

---
+++ src/main/java/br/com/ufcg/demo/ApiGeneratorExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.ufcg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeneratorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeneratorExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ufcg/demo/ApiGeneratorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufcg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeneratorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
